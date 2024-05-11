39392c055c20384ab9220fd7b61a6c4382734305
Võ Trung Hoàng Hưng <21120011@student.hcmus.edu.vn>
2024-05-11 20:52:54 +0700

    final commit

---
+++ routes/task1Route.js
@@ -0,0 +1,8 @@
+const express = require('express');
+const route = express.Router();
+const controller = require('../controllers/controller');
+
+
+route.get('/', controller.task1);
+
+module.exports = route;
---
+++ routes/task2Route.js
@@ -0,0 +1,9 @@
+
+const express = require('express');
+const route = express.Router();
+const controller = require('../controllers/controller');
+
+route.get('/', controller.task2);
+route.post('/', controller.postTask2);
+
+module.exports = route;
